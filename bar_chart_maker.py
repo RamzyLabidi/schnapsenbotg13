5aa8e5e51e2a495217815662491f71109b4a5f70
Ramzyy3 <63471830+Ramzyy3@users.noreply.github.com>
2021-01-18 17:52:54 +0100

    Add files via upload
    experiment N°1: win ratio

---
+++ bar_chart_maker.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+# heights of bars
+f = open("tournament_results.txt", "r").read()
+lines = f.splitlines()
+bot_names = []
+wins = []
+for line in lines:
+    line_split = line.split(".")
+    bot_name = line_split[1]
+    bot_names.append(bot_name)
+    last_part = line_split[-1]
+    last_part_split = last_part.split()
+    win_number = last_part_split[-1]
+    wins.append(int(win_number))
+
+height = wins
+left = bot_names
+# labels for bars
+for bot in bot_names:
+    tick_label = bot_names
+
+# plotting a bar chart
+plt.bar(left, height, tick_label=tick_label,
+        width=0.5, color=['red', 'pink'])
+
+# naming the x-axis
+plt.xlabel('wins')
+# naming the y-axis
+plt.ylabel('bots')
+# plot title
+plt.title('My bar chart!')
+
+# function to show the plot
+plt.show()
+
+plt.savefig('our_experiment.pdf')
